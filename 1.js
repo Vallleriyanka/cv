3fe57f3703f58e04fac2ed9ae1463f03a33dbfea
Lera <lera.ostanina.04@bk.ru>
2024-12-04 15:44:05 +0300

    rew

--- 1.js
+++ 1.js
@@ -1,5 +1,20 @@
-function openWindow() {
-    alert("Заебал, закрой");
- }
+// const text = "Привет, меня зовут Лера!";
 
- 
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     typeWrite();
+// });
+
+// function typeWrite () {
+// for (let i = 0; i<text.length; i++) {
+
+//     let interval = setTimeout (() => {
+//         document.getElementById("title").innerHTML += text.charAt(i);
+//     }, 80*(i+1))
+      
+// }
+// }
+
+
+
+    
